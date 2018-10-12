606dcaa320348c9a6f40b4e594428eebe81f5b9a
QuotePilgrim <caio.psv@gmail.com>
2018-10-12 01:09:07 -0300

    Reorganized the code. Added keybinding settings.

--- midibox360.py
+++ midibox360.py
@@ -5,7 +5,7 @@ import mido
 import pygame
 
 default_config = """
-# Choose which port midiBox360 ouputs to. Must be inside quotation marks
+# Choose which port midiBox360 outputs to. Must be inside quotation marks
 # An empty string can be used to choose the system's default port
 port = ""
 
@@ -20,7 +20,30 @@ channel = 4
 # Select a note to be used as the root note. Can be a number from 0 to 127
 # 60 = middle C
 base_note = 60
+
+[controls]
+# Change which buttons the face buttons are mapped to
+# Valid values: "a", "b", "x", "y".
+a_button = "a"
+b_button = "b"
+x_button = "x"
+y_button = "y"
 """
+
+def msg(diastep, semitone):
+    return mido.Message('note_on', channel=channel, note=(base_note + semitone\
+                        + 12 * octave + notes[play + diastep + mode]) % 128)
+
+def buttonmap(button):
+    if button == "a":
+        button = 0
+    if button == "b":
+        button = 1
+    if button == "x":
+        button = 2
+    if button == "y":
+        button = 3
+    return button
 
 try:
     config_file = open('config.toml', 'r')
@@ -43,6 +66,10 @@ mode = config['mode'] - 1
 channel = config['channel'] - 1
 base_note = config['base_note'] - notes[mode]
 port = config['port']
+a_btn = buttonmap(config['controls']['a_button'])
+b_btn = buttonmap(config['controls']['b_button'])
+x_btn = buttonmap(config['controls']['x_button'])
+y_btn = buttonmap(config['controls']['y_button'])
 
 if port == '':
     outport = mido.open_output()
@@ -53,10 +80,6 @@ done = False
 screen = pygame.display.set_mode([256, 128])
 pygame.display.set_caption("midiBox360")
 
-def msg (add):
-    return mido.Message('note_on', channel=channel, note=(base_note +\
-                        12 * octave + notes[play + add + mode]) % 128)
-
 while done==False:
     joystick_count = pygame.joystick.get_count()
 
@@ -64,26 +87,20 @@ while done==False:
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
 
+    play = 0
+    octave = 0
+    if joystick.get_axis(2) > 0.8:
+        play += 1
+    if joystick.get_button(4) == 1:
+        play += 2
+    if joystick.get_button(5) == 1:
+        play += 4
+    if joystick.get_axis(0) > 0.8:
+        octave = 1
+    if joystick.get_axis(0) < -0.8:
+        octave = -1
+
     for event in pygame.event.get():
-        root = joystick.get_button(0) or joystick.get_button(1)\
-               or joystick.get_button(2)
-        chord = joystick.get_button(1) or joystick.get_button(2)
-        seventh = joystick.get_button(1)
-        set_mode = joystick.get_button(3)
-
-        play = 0
-        octave = 0
-        if joystick.get_axis(2) > 0.8:
-            play += 1
-        if joystick.get_button(4) == 1:
-            play += 2
-        if joystick.get_button(5) == 1:
-            play += 4
-        if joystick.get_axis(0) > 0.8:
-            octave = 1
-        if joystick.get_axis(0) < -0.8:
-            octave = -1
-
         if event.type == pygame.QUIT:
             done=True
 
@@ -98,16 +115,47 @@ while done==False:
                     base_note = (base_note - 12) % 128
 
         if event.type == pygame.JOYBUTTONDOWN:
-            if root:
-                outport.send(msg(0))
-            if chord:
-                outport.send(msg(2))
-                outport.send(msg(4))
-            if seventh:
-                outport.send(msg(6))
-            if set_mode:
-                mode = play % 7
-                base_note = config['base_note'] - notes[mode]
+            root = joystick.get_button(a_btn) or joystick.get_button(b_btn)\
+                   or joystick.get_button(x_btn)
+            chord = joystick.get_button(b_btn) or joystick.get_button(x_btn)
+            seventh = joystick.get_button(b_btn)
+            set_mode = joystick.get_button(y_btn)
+            chord_mode = joystick.get_axis(1) < -0.2
+            maj_chord = joystick.get_button(y_btn)
+            min_chord = joystick.get_button(a_btn)
+            dom_chord = joystick.get_button(b_btn)
+            dim_chord = joystick.get_button(x_btn)
+
+            if not chord_mode:
+                if root:
+                    outport.send(msg(0, 0))
+                if chord:
+                    outport.send(msg(2, 0))
+                    outport.send(msg(4, 0))
+                if seventh:
+                    outport.send(msg(6, 0))
+                if set_mode:
+                    mode = play % 7
+                    base_note = config['base_note'] - notes[mode]
+            else:
+                if maj_chord:
+                    outport.send(msg(0,0))
+                    outport.send(msg(0,4))
+                    outport.send(msg(0,7))
+                elif min_chord:
+                    outport.send(msg(0,0))
+                    outport.send(msg(0,3))
+                    outport.send(msg(0,7))
+                elif dom_chord:
+                    outport.send(msg(0,0))
+                    outport.send(msg(0,4))
+                    outport.send(msg(0,7))
+                    outport.send(msg(0,10))
+                elif dim_chord:
+                    outport.send(msg(0,0))
+                    outport.send(msg(0,3))
+                    outport.send(msg(0,6))
+                    outport.send(msg(0,9))
 
         if event.type == pygame.JOYBUTTONUP:
             for i in range(0, 127):
